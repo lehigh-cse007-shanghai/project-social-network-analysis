f93e5a6d9aee12f7401c41eb7593f4b1d2788005
Fabien Pfaender <fabien.pfaender@me.com>
2020-12-02 12:49:35 +0800

    project commit

---
+++ src/main/java/SocialNetworkAnalysis.java
@@ -0,0 +1,17 @@
+public class SocialNetworkAnalysis {
+
+    public static void main(String[] args) {
+        String questionStatus = "XX";
+        System.out.print("For the STATUS I chose question " + questionStatus + " and my answer is: ");
+        System.out.println("NONE");
+
+        String questionUser = "XX";
+        System.out.print("For the USER I chose question "+ questionUser + " and my answer is: ");
+        System.out.println("NONE");
+
+        String questionLocation = "XX";
+        System.out.print("For the LOCATION I chose question " + questionLocation + " and my answer is: ");
+        System.out.println("NONE");
+
+    }
+}
